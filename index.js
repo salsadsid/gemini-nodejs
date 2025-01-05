ae94bd2edf351518c3f1ba55430041d28baac89d
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2025-01-05 13:37:26 +0600

    port configuration removed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import express from "express";
 import fetch from "node-fetch";
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
@@ -72,9 +72,7 @@ app.post("/api/prompts", async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running.`);
-});
+app.listen();
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-pro",
